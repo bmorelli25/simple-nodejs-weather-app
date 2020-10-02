95376c5369e1871406dcbaa686d686918be4676c
Antar Mukhopadhaya <37949912+arun199941@users.noreply.github.com>
2020-10-02 09:23:03 +0530

    Update Server.js
    Some minor changes for better performance

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const port = 3000;
 const app = express()
 
 const apiKey = '*****************';
@@ -32,6 +33,6 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`)
 })
